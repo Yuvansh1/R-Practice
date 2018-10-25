602fde8c11a213846bf60e226c215dcd5e1a6d46
unknown <yuvansh2@gmail.com>
2018-10-25 18:22:20 +0530

    R Datatypes

---
+++ Datatypes.R
@@ -0,0 +1,33 @@
+#Tip:- R Language bydefault store value's datatype in double
+
+#integer Type
+
+x <- 2L
+
+#To check datatype, we use typeof
+
+typeof(x)
+
+#Double Type
+
+y <- 2.5
+typeof(y)
+
+#Complex Type
+
+z <- 3 + 2i
+typeof(z)
+
+#character
+
+a <- "h"
+typeof(a)
+
+#Logical
+q1 <- T 
+#or 
+q1 <- FALSE
+typeof(q1)
+
+
+
