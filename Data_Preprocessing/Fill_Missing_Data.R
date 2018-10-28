e0321e9ad02d6228ed379d1fb77625ecbcf9870e
unknown <yuvansh2@gmail.com>
2018-10-28 22:27:44 +0530

    Next Commit

---
+++ Data_Preprocessing/Fill_Missing_Data.R
@@ -0,0 +1,27 @@
+#Importing the Dataset
+dataset = read.csv('Data.csv')
+View(dataset)
+
+#Taking Care of  Missing Data
+
+#We would replace missing cell in Age Coumn with mean of Age Column Data
+
+#Syntax of ifelse() is ifelse(condition,if True Output,else output)
+
+dataset$Age =  ifelse(is.na(dataset$Age),
+                      ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                      dataset$Age)
+
+#We would replace missing cell in Salary Coumn with mean of Salary Column Data
+
+dataset$Salary =  ifelse(is.na(dataset$Salary),
+                      ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                      dataset$Salary)
+                      
+#Here is.NA(NA is function) tells us if the value of the function is missing or not
+
+#Dollar Sign($) is used to check all the values in Age Column is missing or not
+
+#If the condition is True that means value is missing
+
+#ave is average
