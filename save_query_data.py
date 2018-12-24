f1bc6699ab27f21bd4bd42d290fe1ac6776a126a
unknown <yuvansh2@gmail.com>
2018-12-24 17:34:04 +0530

    Save Query Data using Python

---
+++ save_query_data.py
@@ -0,0 +1,17 @@
+import mysql.connector
+import csv
+
+cnx = mysql.connector.connect(user='yuvansh'
+                              , password='****'
+                              ,host='localhost'
+                              ,database='***')
+cursor = cnx.cursor()
+
+QUERY = "select * from BI.FGVGE limit 10');"
+
+cursor.execute(QUERY)
+result=cursor.fetchall()
+c = csv.writer(open('C:/Users/User/Desktop/Second_One.csv', 'w', newline=''))
+c.writerow([result[0] for result in cursor.description])
+for x in result:
+    c.writerow(x)
