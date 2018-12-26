5e5b79c5434533c251a512aafc07c6af1489795e
unknown <yuvansh2@gmail.com>
2018-12-26 12:47:36 +0530

    'Save_Data'

--- save_query_data.py
+++ save_query_data.py
@@ -1,17 +1,23 @@
 import mysql.connector
 import csv
 
-cnx = mysql.connector.connect(user='yuvansh'
-                              , password='****'
-                              ,host='localhost'
-                              ,database='***')
+cnx = mysql.connector.connect(user='***'
+                             , password='***'
+                             ,host='***'
+                             ,database='***')
 cursor = cnx.cursor()
 
-QUERY = "select * from *** limit 10');"
-
-cursor.execute(QUERY)
-result=cursor.fetchall()
-c = csv.writer(open('C:/Users/User/Desktop/Second_One.csv', 'w', newline=''))
-c.writerow([result[0] for result in cursor.description])
-for x in result:
-    c.writerow(x)
+QUERY = """
+select * from *** limit 10;
+"""
+try:
+    cursor.execute(QUERY)
+    result=cursor.fetchall()
+    c = csv.writer(open('C:/Users/User/Desktop/Office Work/26-12-2018/***.csv', 'w', newline=''))
+    c.writerow([result[0] for result in cursor.description])
+    for x in result:
+        c.writerow(x)
+    cursor.close()
+    cnx.close()
+except (mysql.connector.Error, mysql.connector.Warning) as e:
+    print(e)
